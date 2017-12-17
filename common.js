05c88dbaa6b347620391ec3ddd93552c039aa908
xu1718191411 <xu1718191411@gmail.com>
2017-12-17 19:09:09 +0900

    good good study day day up

---
+++ common.js
@@ -0,0 +1,7 @@
+var APPLICATION_KEY = "e49d2a7325b1de31930285ecfceb148c6c07bd1b0ff87cc9ccb3e6836aeb7151";
+var CLIENT_KEY = "4c5ff454f2fbf0bb0ed2b4088fa54f37602aba3744e6d6bc079a3886c44aa4e3";
+var ncmb = new NCMB(APPLICATION_KEY, CLIENT_KEY);
+
+//var module = angular.module('apps', []);
+
+var module = ons.bootstrap()
